abe26a1907c8a33fab7145a22be8d303388cc409
Spatulox <M78stormtrooper@laposte.net>
2024-04-02 12:54:01 +0200

    init commit 2

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'VZ0N5U7V2FT71RAE85Q8', // TODO
+    secretAccessKey: 'KVI0J3KKYd0Y5rRNh9ksznpEhHDaLBzLWPlNeou0', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-42453', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
